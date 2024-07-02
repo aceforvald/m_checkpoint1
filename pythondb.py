4c090f53232aca14e77137cac69c9a443e72156c
anna <anna@fornaeus.com>
2024-07-02 06:40:41 -0700

    all the stuff

--- pythondb.py
+++ pythondb.py
@@ -22,14 +22,22 @@ def read_dict():
 def add_dict(word, translation):
     dbconn = db_connection()
     cur = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("SELECT id, word, translation FROM dictionary;")
-    rows = cur.fetchall()
+    cur.execute('INSERT INTO dictionary (word, translation) VALUES (%s, %s);', (word, translation))
     cur.close()
     dbconn.close()
-    return rows
+    print(f'you added {new_word}, translates to {translation}')
+
 
 def delete_dict(word):
-    pass
+    dbconn = db_connection()
+    cur = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    delete_script = 'DELETE FROM dictionary WHERE word = %s'
+    delete_record = (word,)
+    cur.execute(delete_script, delete_record)
+    cur.close()
+    dbconn.close()
+    print(f'you deleted {word}')
+
 
 while True: ## REPL - Read Execute Program Loop
     cmd = input("Command: ")
@@ -40,11 +48,12 @@ while True: ## REPL - Read Execute Program Loop
     elif cmd == "add":
         new_word = input("English word to add: ")
         translation = input("Swedish translation: ")
-        print(f'you added {new_word}, translates to {translation}')
+        add_dict(new_word, translation)
 
     elif cmd == "delete":
         delete_word = input("English word to delete: ")
-        print(f'you want to delete {delete_word}')
+        delete_dict(delete_word)
+
     elif cmd == "quit":
         exit()
     else:
