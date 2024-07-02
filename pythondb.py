9e32a2b6ce0f0b9119c85e0de8e1ecb431c5fbd5
anna <anna@fornaeus.com>
2024-07-02 06:12:55 -0700

    Confirm connection to db

--- pythondb.py
+++ pythondb.py
@@ -1,25 +1,31 @@
 import psycopg2
+import psycopg2.extras
 
 def db_connection():
     return psycopg2.connect(
-        host="localhost"
+        host = 'localhost',
+        database = 'dictdb',
+        port = '5432',
+        user = 'postgres',
+        password = 'anna'
 )
 
 def read_dict():
     dbconn = db_connection()
-    cur = dbconn.cursor()
+    cur = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     dbconn.close()
     return rows
 
+#read_dict()
 
 while True: ## REPL - Read Execute Program Loop
     cmd = input("Command: ")
 
     if cmd == "list":
-        
+        print(read_dict())
     elif cmd == "quit":
         exit()
     else:
