ddf131c7d7387fa0c7ed61a47adc2c017c558f5d
anna <anna@fornaeus.com>
2024-07-02 06:24:58 -0700

    Add control loop statement bone structure

--- pythondb.py
+++ pythondb.py
@@ -19,13 +19,32 @@ def read_dict():
     dbconn.close()
     return rows
 
-#read_dict()
+def add_dict(word, translation):
+    dbconn = db_connection()
+    cur = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT id, word, translation FROM dictionary;")
+    rows = cur.fetchall()
+    cur.close()
+    dbconn.close()
+    return rows
+
+def delete_dict(word):
+    pass
 
 while True: ## REPL - Read Execute Program Loop
     cmd = input("Command: ")
 
     if cmd == "list":
         print(read_dict())
+
+    elif cmd == "add":
+        new_word = input("English word to add: ")
+        translation = input("Swedish translation: ")
+        print(f'you added {new_word}, translates to {translation}')
+
+    elif cmd == "delete":
+        delete_word = input("English word to delete: ")
+        print(f'you want to delete {delete_word}')
     elif cmd == "quit":
         exit()
     else:
